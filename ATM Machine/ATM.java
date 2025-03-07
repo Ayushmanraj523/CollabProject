49b9c9313ebdc949c0bd9d455f72b0a0086050ee
SAHIL KUMAR <sahilkum097@gmail.com>
2025-03-08 01:35:59 +0530

    Create a new class name banksys, create variable for balance, function

--- ATM Machine/ATM.java
+++ ATM Machine/ATM.java
@@ -1,6 +1,29 @@
 package ATM Machine;
-
-public class ATM {
-    double Withdraw , checkBalance , Deposit , 
+class BankingSystem {
+    private double balance;
+    
+    public void deposite(double amount) {
+           if(amount > 0) {
+               balance += amount;
+               System.out.println("desposite succesfully" + balance);
+           }
+        else {
+            System.out,println("Invalid ammount");
+        }
+    }
+    public void Withdraw() {
+    }
+    public void CheckBalance() {
+    }
+    
+    
     
 }
+
+
+public class ATM {
+    public static void main(String []args){
+        
+    
+    
+}}
