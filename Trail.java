34f030acf2c689d32a3c283203a9905fe516514f
Ayushman Raj <ayushmanraj925@gmail.com>
2025-03-07 22:25:24 +0530

    update code

--- Trail.java
+++ Trail.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Trail {
+
+    public void f1(int x) {
+        System.out.println("Hello, Everyone! i am function");
+    }
+    Trail(){
+        System.out.println("hey everyone i am contructor");
+    }
+
     public static void main(String[] args) {
+        System.out.println("Enter the number : ");
+        Scanner sc=new Scanner(System.in);
+        int x =sc.nextInt();
+
+        Trail ob=new Trail();
+        ob.f1(x);
+        
+
+
+    
         System.out.println("Hello, Everyone!");
     }
 }
