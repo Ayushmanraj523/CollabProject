1a4d323064d162f79ff47922ab0588473d380c5a
Durga Shankar Mahto <durgashankarmahto7@gmail.com>
2025-03-08 01:58:00 +0530

    2nd funtion create & spelling mistakes corrected

--- ATM Machine/ATM.java
+++ ATM Machine/ATM.java
@@ -2,17 +2,20 @@ package ATM Machine;
 class BankingSystem {
     private double balance;
     
-    public void deposite(double amount) {
+    public void deposit(double amount) {
            if(amount > 0) {
                balance += amount;
-               System.out.println("desposite succesfully" + balance);
+               System.out.println("desposit succesfully" + balance);
            }
         else {
             System.out,println("Invalid ammount");
         }
     }
-    public void Withdraw() {
-    }
+    public void Withdraw(double amount) {
+        if(amount <= balance){
+            balance -= amount;
+            System.out.println("Withdrawal Successfull\n" + "Remaining Balance" + balance);
+        }
     public void CheckBalance() {
     }
     
